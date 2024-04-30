436484c4b72cfb69b58841024ddfec62569f5066
AlexEatDonut <alexeatdonut@gmail.com>
2024-04-30 14:24:50 +0200

    commited project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # onlyhuman
+
 Useless website that wastes internet data.
+Rag'N'Bones owns the rights to the images.
---
+++ script.js
@@ -0,0 +1,7 @@
+// let human = document.querySelector(".human");
+
+function fd(frameName) {
+  let human = document.querySelector(".human");
+  human.src = `./img/${frameName}.jpeg`;
+  // console.log("./img/${frameName}.jpeg");
+}
