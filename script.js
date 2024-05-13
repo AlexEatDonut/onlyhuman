8cf6294222feb0395b7b34578b5316b099d51407
AlexEatDonut <alexeatdonut@gmail.com>
2024-05-13 10:04:44 +0200

    replaced jpeg files with webp

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 
 function fd(frameName) {
   let human = document.querySelector(".human");
-  human.src = `./img/${frameName}.jpeg`;
+  human.src = `./img/${frameName}.webp`;
   // console.log("./img/${frameName}.jpeg");
 }
